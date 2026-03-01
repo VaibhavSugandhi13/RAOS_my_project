4e9348cf9b0bcf45563907b3becb88d6c11fa9e7
Vaibhav Sugandhi <vaibhavjisugandhiji6999@gmail.com>
2026-03-01 13:25:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -96,4 +96,6 @@ def generate_report(sub_domain):
     return response
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    # Render assigns a port via environment variable
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
